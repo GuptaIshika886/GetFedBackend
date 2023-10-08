6e25dd6bc5de057d728897ed8bbd639546666f69
Ishika Gupta <ishikag886@gmail.com>
2023-10-09 01:31:46 +0300

    modifying requirement file

--- getfeddjangoss/urls.py
+++ getfeddjangoss/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('https://getfed-backend.up.railway.app/admin/login/?next=/admin/admin/', admin.site.urls),
     path('',views.index,name='index')
     # path('userDetails/',views.usersDetail),
     # path('canteenDetail/',views.canteenDetail),
